164b1b8d06c9f300fb44842368c04a8fc4944d85
PillowGit <goldentaco727@gmail.com>
2024-03-04 12:23:06 -0800

    Task 1 added

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+// TODO:
+// Write a function called "PrintString" that:
+// 1. Takes in a string by reference
+// 2. Prints the string
+
+int main(int argc, char *argv[]) {
+  // TODO:
+  // Declare a string variable
+
+  // TODO:
+  // Get the value of the string variable from the user
+
+  // TODO:
+  // Call the PrintString function
+
+  return 0;
+}
